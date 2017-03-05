302f97b05acf9da983d09eb28e6aa7c337840a67
Jeremy Ward <jeremy@3five.com>
2017-03-05 14:14:08 -0600

    Update anchor hrefs in index.php

--- app/index.php
+++ app/index.php
@@ -8,12 +8,12 @@
 <p><strong>User views</strong><br>
 	<a href="select_roster.php">MLB Rosters (Web user view)</a><br>
 	<a href="free_agent_summary.php">Free Agents (General manager view)</a><br>
-	<a href="ticket_search.html">Ticket search (Stadium user view)</a><br>
-	<a href="ump_assignments.html">Umpire Game Assignments</a></p>
+	<a href="ticket_search.php">Ticket search (Stadium user view)</a><br>
+	<a href="ump_assignments.php">Umpire Game Assignments</a></p>
 
 <p><strong>Database entry</strong><br>
-	<a href="game_new.html">Enter new game</a><br>
-	<a href="player_new.html">Enter new player</a></p>
+	<a href="game_new.php">Enter new game</a><br>
+	<a href="player_new.php">Enter new player</a></p>
 
 </body>
 </html>
