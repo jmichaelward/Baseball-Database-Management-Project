98a72301cf410ebfedeca2ce7b04e1e4bd016042
Jeremy Ward <jeremy@3five.com>
2017-03-05 16:13:57 -0600

    Add closing paragraph tag and clean up variable declaration in get_roster.php

--- app/get_roster.php
+++ app/get_roster.php
@@ -2,16 +2,16 @@
 include_once './src/class.query.php';
 include_once './src/class.team.php';
 
-$team_id = $_REQUEST['team'];
-$team    = new Team( ( new Query )->team_by_id( $team_id ) );
-
+$team_id   = $_REQUEST['team'];
+$team      = new Team( ( new Query )->team_by_id( $team_id ) );
 $positions = [
 	'Pitchers'           => 'P',
 	'Catchers'           => 'C',
 	'Infielders'         => [ '1B', '2B', 'SS', '3B', 'IF' ],
 	'Outfielders'        => [ 'LF', 'CF', 'RF' ],
 	'Designated Hitters' => 'DH'
-]
+];
+$db        = DB::get_instance();
 ?>
 
 <!DOCTYPE html>
@@ -25,37 +25,36 @@ $positions = [
 <body>
 <div id="container">
 	<h1><?php echo $team->name(); ?></h1>
-	<?php $db = DB::get_instance(); ?>
+	<p>Click <a href="select_roster.php">here</a> to select another team's roster.</p>
 
-	<p>Click <a href="select_roster.php">here</a> to select another team's roster.
-		<?php foreach ( $positions as $section => $position ) :
-		?>
-	<table>
-		<tr>
-			<h2><?php echo $section; ?></h2>
-			<th>Position</th>
-			<th>Player Name</th>
-		</tr>
-		<?php
-		$where  = is_array( $position ) ? ' IN ("' . implode( '", "', $position ) . '")' : " = '{$position}'";
-		$query  = "SELECT * FROM players
+	<?php foreach ( $positions as $section => $position ) : ?>
+		<table>
+			<tr>
+				<h2><?php echo $section; ?></h2>
+				<th>Position</th>
+				<th>Player Name</th>
+			</tr>
+
+			<?php
+			$where  = is_array( $position ) ? ' IN ("' . implode( '", "', $position ) . '")' : " = '{$position}'";
+			$query  = "SELECT * FROM players
 				LEFT JOIN teams
 				ON players.team_id = teams.team_id
 				WHERE position {$where}
 				AND players.team_id = '$team_id'
 				ORDER BY position, player_lastname";
-		$result = mysqli_query( $db->connection(), $query );
+			$result = mysqli_query( $db->connection(), $query );
 
-		while ( $row = mysqli_fetch_assoc( $result ) ) {
-			echo "\n<tr><td>" . $row['position'] . "</td>";
-			echo "<td>" . $row['player_firstname'] . "&nbsp" . $row['player_lastname'] . "</td>";
-			echo "<td>" . $row['team_name'] . "</td></tr>";
-		}
+			while ( $row = mysqli_fetch_assoc( $result ) ) {
+				echo "\n<tr><td>" . $row['position'] . "</td>";
+				echo "<td>" . $row['player_firstname'] . "&nbsp" . $row['player_lastname'] . "</td>";
+				echo "<td>" . $row['team_name'] . "</td></tr>";
+			}
 
-		$db->disconnect();
-		?>
-	</table>
-<?php endforeach; ?>
+			$db->disconnect();
+			?>
+		</table>
+	<?php endforeach; ?>
 </div>
 </body>
 </html>
